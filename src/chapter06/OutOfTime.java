6c20d2d5d4faf53639555939a9882443eb28ad80
Chen <77219776@qq.com>
2017-07-19 10:21:17 +0800

    1

---
+++ src/chapter06/OutOfTime.java
@@ -0,0 +1,27 @@
+package chapter06;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class OutOfTime {
+
+    public static void main(String[] args) throws Exception {
+        Timer timer = new Timer();
+        timer.schedule(new ThrowTask(),1);
+        TimeUnit.SECONDS.sleep(1);
+        timer.schedule(new ThrowTask(),1);
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("程序退出。。。");
+
+    }
+
+    static class ThrowTask extends TimerTask{
+
+        @Override
+        public void run() {
+            System.out.println("定时任务运行。。。");
+            throw new RuntimeException();
+        }
+    }
+}
